98c1f8f40c8ded36fcca61c17f02345117ccc50f
IceMortyGod <76694723+IceMortyGod@users.noreply.github.com>
2021-01-03 16:18:12 +0530

    Add files via upload

---
+++ IceMorty Pro Calculator.py
@@ -0,0 +1,66 @@
+print("Welcome To Sub To Icemorty Facking Calculator:)")
+
+
+x = int(input('Enter the first number pls: '))
+y = int(input('Enter the second number: '))
+yes = ("Good Job Facker :)")
+no = ("fuck of facker 👿")
+
+def add(x, y):
+    print(x + y)
+
+
+def subtract(x, y):
+    print(x - y)
+
+
+def multiply(x, y):
+    print(x * y)
+
+
+def divide(x, y):
+    print(x / y)
+
+instructions = """
+Hey Noob Dumbass Wanna ADD? Ok Then Type   A
+Uh Facker Wanna SUBTRACT? Ok Nub Type   S
+STFU Nub Head Wanna MULTIPLY? Ok Facker Type    M
+Uh OH Facker Wanna DIVIDE? OK Dummy Type     D
+"""
+print( instructions)
+
+operation = input('Type letter here: ')
+
+if operation == "A":
+    add(x, y)
+
+elif operation == "S":
+    subtract(x, y)
+
+elif operation == "M":
+    multiply(x, y)
+
+elif operation == "D":
+    divide(x, y)
+
+else:
+    print("yo facker this is wrong ya nub  :(")
+
+
+operation = input("Will you sub to icemorty Type yes Or no or  facker die: ")
+
+
+
+if operation == "yes":
+    input(yes)
+
+
+elif operation == "no":
+  input(no)
+
+else:
+    print("bruh facker i said to type yes or no not something else dumbass")
+
+
+
+print("Special Thanks To Lolipop Link To HIs WEbsite Will Be Given Soon :) ")
